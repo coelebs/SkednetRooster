9480c0a1d67f3b0823e05bd2e15edfb0ea2b5f31
Vincent Kriek <vincent@vincentkriek.nl>
2010-12-18 16:26:42 +0100

    Check remote

--- gen/com/vin/R.java
+++ gen/com/vin/R.java
@@ -14,18 +14,27 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int badge_input=0x7f050001;
-        public static final int password_input=0x7f050002;
-        public static final int submit=0x7f050003;
-        public static final int text=0x7f050000;
+        public static final int badge_input=0x7f060001;
+        public static final int logout=0x7f060004;
+        public static final int password_input=0x7f060002;
+        public static final int submit=0x7f060003;
+        public static final int text=0x7f060000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int login=0x7f030000;
+        public static final int row=0x7f030001;
+    }
+    public static final class menu {
+        public static final int menu=0x7f050000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int badge=0x7f040002;
+        public static final int choose_location=0x7f040005;
+        public static final int exit=0x7f040008;
         public static final int hello=0x7f040000;
+        public static final int logout=0x7f040007;
+        public static final int ok=0x7f040006;
         public static final int password=0x7f040003;
         public static final int submit=0x7f040004;
     }
